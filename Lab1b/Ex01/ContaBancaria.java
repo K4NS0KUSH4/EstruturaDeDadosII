54941c8f9350f7210eac7ff8967970e481ba2f65
K4NS0KUSH4 <pedro.he7721@gmail.com>
2023-08-20 20:20:36 -0300

    Finalizando o arquivo ContaBancaria.java do Exercício 01

--- Lab1b/Ex01/ContaBancaria.java
+++ Lab1b/Ex01/ContaBancaria.java
@@ -13,13 +13,51 @@ public class ContaBancaria {
         this.saldo = saldoArg;
     }
 
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String clienteArg) {
+        cliente = clienteArg;
+    }
+
+    public int getNroConta() {
+        return nroConta;
+    }
+
+    public void setNroConta(int nroContaArg) {
+        nroConta = nroContaArg;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldoArg) {
+        saldo = saldoArg;
+    }
+
     public void sacar(float valor) {
         if(saldo < 0) {
-            System.out.println("Saldo negativo. Não é possível realizar a operação.");
+            System.out.println("Não é possível realizar a operação.\nO saldo atual da conta é negativo.");
+        } else {
+            if(saldo - valor < 0) {
+                System.out.println("Não é possível realizar a operação.\nO saldo da conta após a operação é negativo.");
+            } else {
+                saldo -= valor;
+                System.out.println("Foram sacados R$" + valor + ".\nSaldo atual: R$" + saldo);
+            }
         }
     }
 
     public void depositar(float valor) {
+        saldo += valor;
+        System.out.println("Foram depositados R$" + valor + ".\nSaldo atual: R$" + saldo);
+    }
 
+    @Override
+    public String toString() {
+        return "Nome: " + cliente + " | Número: " + nroConta +
+        " | Saldo: R$" + saldo + ";";
     }
 }
