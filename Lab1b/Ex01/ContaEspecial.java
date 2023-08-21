d32fbb53fcd67047b42896037d4b0177459dc332
K4NS0KUSH4 <pedro.he7721@gmail.com>
2023-08-20 21:08:04 -0300

    Adicionando classe ContaEspecial

--- Lab1b/Ex01/ContaEspecial.java
+++ Lab1b/Ex01/ContaEspecial.java
@@ -1,3 +1,30 @@
-public class ContaEspecial {
-    
+public class ContaEspecial extends ContaBancaria {
+    private float limite;
+
+    public ContaEspecial() {
+        this(0.0f);
+    }
+
+    public ContaEspecial(float limiteArg) {
+        super("\0", 0, 0.0f);
+        this.limite = limiteArg;
+    }
+
+    public ContaEspecial(String clienteArg, int nroContaArg, float saldoArg, float limiteArg) {
+        super(clienteArg, nroContaArg, saldoArg);
+        this.limite = limiteArg;
+    }
+
+    public float getLimite() {
+        return limite;
+    }
+
+    public void setLimite(float limiteArg) {
+        this.limite = limiteArg;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Limite: R$" + limite;
+    }
 }
