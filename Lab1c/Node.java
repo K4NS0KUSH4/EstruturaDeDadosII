21489f40335f36948c5c87dadcb24f1972ecf0ae
K4NS0KUSH4 <pedro.he7721@gmail.com>
2023-09-10 14:26:40 -0300

    Adicionando arquivo Node.java

---
+++ Lab1c/Node.java
@@ -0,0 +1,118 @@
+public class Node {
+
+    private String data;
+    private Node parent;
+    private Node left;
+    private Node right;
+
+    public Node() {
+        this.data = null;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(String data) {
+        this.data = data;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(String data, Node parent) {
+        this.data = data;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public boolean isRoot() {
+        return (this.left != null || this.right != null);
+    }
+
+    public boolean isLeaf() {
+        return (this.left == null || this.right == null);
+    }
+
+    // getDegree() Retorna o grau do nó (int)Numero de filhos
+    public int getDegree() {
+        if (this.left == null && this.right == null) {
+            return 0;
+        }
+        if (this.left != null && this.right == null || this.left == null && this.right != null) {
+            return 1;
+        }
+        return 2;
+    }
+
+    // Arrumar
+    public int getLevel(Node raiz) {
+        Node head = this;
+        int contador = 0;
+        while (head != raiz) {
+            if (head.getParent() != null) {
+                head = head.getParent();
+                ++contador;
+            }
+        }
+        return contador;
+    }
+
+    public int getHeight(Node elemento) {
+        if (elemento == null) {
+            return -1;
+        }
+        int h_left = 0;
+        int h_right = 0;
+        if (elemento.getLeft() != null) {
+            h_left = getHeight(elemento.getLeft());
+        }
+        if (elemento.getRight() != null) {
+            h_right = getHeight(elemento.getLeft());
+        }
+        if (h_left > h_right) {
+            return h_left + 1;
+        }
+        return h_right + 1;
+    }
+
+    @Override
+    public String toString() {
+        return " | " + data + " | " +
+                "\n | " + (left != null ? left.getData() : null) + " | " +
+                " |  " + (right != null ? right.getData() : null) + " | ";
+    }
+
+}
