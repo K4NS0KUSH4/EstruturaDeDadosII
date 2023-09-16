6eb547dd305af25189ef5093b5eb2da92056bd83
K4NS0KUSH4 <pedro.he7721@gmail.com>
2023-09-16 20:56:20 -0300

    Primeiros métodos da classe BST

--- Lab1d/src/BST.java
+++ Lab1d/src/BST.java
@@ -3,6 +3,30 @@
  * NOME: Pedro Henrique Araujo Farias   TIA: 32246994
 */
 
-public class BST extends BinaryTree{
-    
+public class BST extends BinaryTree {
+
+    public BST() {
+        super(null);
+    }
+
+    public BST(Node root) {
+        super(root);
+    }
+
+    public Node search(Node rootBST, String data) {
+        if(rootBST == null) {
+            return null;
+        }
+
+        if(rootBST.getData().compareTo(data) < 0) {
+            return search(rootBST.getRight(), data);
+        }
+
+        if(rootBST.getData().compareTo(data) > 0) {
+            return search(rootBST.getLeft(), data);
+        }
+
+        return rootBST;
+    }
+
 }
